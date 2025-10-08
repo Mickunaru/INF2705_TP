4f428b1013166754fd74920496affd2873a688b3
Michael Le <orangerasengan25@gmail.com>
2025-10-07 20:15:45 -0400

    Removed unnecessary code from TP2

--- tp2/src/main.cpp
+++ tp2/src/main.cpp
@@ -34,18 +34,14 @@ struct Vertex {
 struct App : public OpenGLApplication
 {
     App()
-        : nSide_(5)
-        , oldNSide_(0)
-        , cameraPosition_(-9.0f, 1.5f, 1.5f)
+        : cameraPosition_(-9.0f, 1.5f, 1.5f)
         , cameraOrientation_(-10.f, -60.0f)
-        , currentScene_(0)
         , isMouseMotionEnabled_(false)
     {
     }
 
     void init() override
     {
-        // Le message expliquant les touches de clavier.
         setKeybindMessage(
             "ESC : quitter l'application." "\n"
             "T : changer de scène." "\n"
@@ -60,16 +56,10 @@ struct App : public OpenGLApplication
             "Espace : activer/désactiver la souris." "\n"
         );
 
-        // Config de base.
         glClearColor(0.7f, 0.7f, 0.7f, 1);
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_CULL_FACE);
         loadShaderPrograms();
-
-        // Partie 1
-        initShapeData();
-
-        // Partie 2
         loadModels();
     }
 
@@ -103,34 +93,18 @@ struct App : public OpenGLApplication
         }
     }
 
-
-    // Appelée à chaque trame. Le buffer swap est fait juste après.
     void drawFrame() override
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        ImGui::Begin("Scene Parameters");
-        ImGui::Combo("Scene", &currentScene_, SCENE_NAMES, N_SCENE_NAMES);
-        ImGui::End();
-
-        switch (currentScene_)
-        {
-        case 0: sceneShape();  break;
-        case 1: sceneModels(); break;
-        }
+        sceneModels();
     }
 
-    // Appelée lorsque la fenêtre se ferme.
     void onClose() override
     {
-        glDeleteProgram(basicSP_);
-        glDeleteProgram(transformSP_);
-        glDeleteVertexArrays(1, &vao_);
-        glDeleteBuffers(1, &vbo_);
-        glDeleteBuffers(1, &ebo_);
+        glDeleteProgram(shaderProgram_);
     }
 
-    // Appelée lors d'une touche de clavier.
     void onKeyPress(const sf::Event::KeyPressed& key) override
     {
         using enum sf::Keyboard::Key;
@@ -151,9 +125,6 @@ struct App : public OpenGLApplication
                 window_.setMouseCursorGrabbed(false);
                 window_.setMouseCursorVisible(true);
             }
-            break;
-        case T:
-            currentScene_ = ++currentScene_ < N_SCENE_NAMES ? currentScene_ : 0;
             break;
         default: break;
         }
@@ -204,7 +175,6 @@ struct App : public OpenGLApplication
         cameraOrientation_.y -= cameraMouvementY * deltaTime_;
         cameraOrientation_.x -= cameraMouvementX * deltaTime_;
 
-        // Keyboard input
         glm::vec3 positionOffset = glm::vec3(0.0);
         const float SPEED = 10.f;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
@@ -250,139 +220,27 @@ struct App : public OpenGLApplication
 
     void loadShaderPrograms()
     {
-        // Partie 1
-        const char* COLOR_VERTEX_SRC_PATH = "./shaders/basic.vs.glsl";
-        const char* COLOR_FRAGMENT_SRC_PATH = "./shaders/basic.fs.glsl";
-
-        basicSP_ = glCreateProgram();
-        GLuint colorVertexShader = loadShaderObject(GL_VERTEX_SHADER, COLOR_VERTEX_SRC_PATH);
-        GLuint colorFragmentShader = loadShaderObject(GL_FRAGMENT_SHADER, COLOR_FRAGMENT_SRC_PATH);
-        glAttachShader(basicSP_, colorVertexShader);
-        glAttachShader(basicSP_, colorFragmentShader);
-        glLinkProgram(basicSP_);
-
-        App::checkProgramLinkingError("basic shader program", basicSP_);
-
-        glDetachShader(basicSP_, colorVertexShader);
-        glDetachShader(basicSP_, colorFragmentShader);
-        glDeleteShader(colorVertexShader);
-        glDeleteShader(colorFragmentShader);
-
-        // Partie 2
         const char* TRANSFORM_VERTEX_SRC_PATH = "./shaders/transform.vs.glsl";
         const char* TRANSFORM_FRAGMENT_SRC_PATH = "./shaders/transform.fs.glsl";
 
-        transformSP_ = glCreateProgram();
+        shaderProgram_ = glCreateProgram();
         GLuint transformVertexShader = loadShaderObject(GL_VERTEX_SHADER, TRANSFORM_VERTEX_SRC_PATH);
         GLuint transformFragmentShader = loadShaderObject(GL_FRAGMENT_SHADER, TRANSFORM_FRAGMENT_SRC_PATH);
-        glAttachShader(transformSP_, transformVertexShader);
-        glAttachShader(transformSP_, transformFragmentShader);
-        glLinkProgram(transformSP_);
+        glAttachShader(shaderProgram_, transformVertexShader);
+        glAttachShader(shaderProgram_, transformFragmentShader);
+        glLinkProgram(shaderProgram_);
 
-        App::checkProgramLinkingError("transform shader program", transformSP_);
+        App::checkProgramLinkingError("transform shader program", shaderProgram_);
 
-        glDetachShader(transformSP_, transformVertexShader);
-        glDetachShader(transformSP_, transformFragmentShader);
+        glDetachShader(shaderProgram_, transformVertexShader);
+        glDetachShader(shaderProgram_, transformFragmentShader);
         glDeleteShader(transformVertexShader);
         glDeleteShader(transformFragmentShader);
 
-        basicMvpUniformLocation_ = glGetUniformLocation(basicSP_, "mvp");
-        basicColorModUniformLocation_ = glGetUniformLocation(basicSP_, "colorMod");
-        transformMvpUniformLocation_ = glGetUniformLocation(transformSP_, "mvp");
-        transformColorModUniformLocation_ = glGetUniformLocation(transformSP_, "colorMod");
-        car_.mvpUniformLocation = glGetUniformLocation(transformSP_, "mvp");
-        car_.colorModUniformLocation = glGetUniformLocation(transformSP_, "colorMod");
-    }
-
-    void generateNgon(std::vector<Vertex>* vertices, std::vector<GLuint>* elements, unsigned int side)
-    {
-        const float RADIUS = 0.7f;
-        const vec4 RED = { 1.f, 0.f, 0.f, 1.f };
-        const vec4 GREEN = { 0.f, 1.f, 0.f, 1.f };
-        const vec4 BLUE = { 0.f, 0.f, 1.f, 1.f };
-        const vec4 WHITE = { 1.f, 1.f, 1.f, 1.f };
-        const float angleInc = 2.f * M_PI / side;
-        *vertices = {};
-        *elements = {};
-
-        vertices->push_back({
-            { 0.f, 0.f, 0.f }, WHITE
-        });
-
-        for (int i = 0; i < side; i++) {
-            const float angle = i * angleInc;
-            const float x = RADIUS * cos(angle);
-            const float y = RADIUS * sin(angle);
-            vec4 color;
-            switch (i % 3) {
-            case 0:
-                color = RED;
-                break;
-            case 1:
-                color = GREEN;
-                break;
-            case 2:
-                color = BLUE;
-                break;
-            }
-
-            vertices->push_back({
-                {x, y, 0.f}, color
-            });
-
-            elements->push_back(0);
-            elements->push_back(i + 1);
-
-            if (i + 2 > side) {
-                elements->push_back(1);
-            }
-            else {
-                elements->push_back(i + 2);
-            }
-        }
-    }
-
-    void initShapeData()
-    {
-        glGenBuffers(1, &vbo_);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
-        glBufferData(GL_ARRAY_BUFFER, vertices_.size() * sizeof(Vertex), vertices_.data(), GL_STATIC_DRAW);
-
-        glGenVertexArrays(1, &vao_);
-        glGenBuffers(1, &ebo_);
-
-        glBindVertexArray(vao_);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements_.size() * sizeof(GLuint), elements_.data(), GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
-
-        glEnableVertexAttribArray(0);
-        glEnableVertexAttribArray(1);
-        glBindVertexArray(0);
-    }
-
-    void sceneShape()
-    {
-        ImGui::Begin("Scene Parameters");
-        ImGui::SliderInt("Sides", &nSide_, MIN_N_SIDES, MAX_N_SIDES);
-        ImGui::End();
-
-        bool hasNumberOfSidesChanged = nSide_ != oldNSide_;
-        if (hasNumberOfSidesChanged)
-        {
-            oldNSide_ = nSide_;
-            generateNgon(&vertices_, &elements_, nSide_);
-            initShapeData();
-        }
-
-        glUseProgram(basicSP_);
-        glBindVertexArray(vao_);
-        glDrawElements(GL_TRIANGLE_FAN, elements_.size(), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        mvpUniformLocation_ = glGetUniformLocation(shaderProgram_, "mvp");
+        colorModUniformLocation_ = glGetUniformLocation(shaderProgram_, "colorMod");
+        car_.mvpUniformLocation = glGetUniformLocation(shaderProgram_, "mvp");
+        car_.colorModUniformLocation = glGetUniformLocation(shaderProgram_, "colorMod");
     }
 
     void drawStreetlights(glm::mat4& projView)
@@ -413,8 +271,8 @@ struct App : public OpenGLApplication
         for (unsigned int i = 0; i < N_STREETLIGHTS; ++i)
         {
             glm::mat4 streetlightMVP = projView * streetlightModelMatrices_[i];
-            glUniformMatrix4fv(transformMvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(streetlightMVP));
-            glUniform3f(transformColorModUniformLocation_, 1.0f, 1.0f, 1.0f);
+            glUniformMatrix4fv(mvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(streetlightMVP));
+            glUniform3f(colorModUniformLocation_, 1.0f, 1.0f, 1.0f);
             streetlight_.draw();
         }
     }
@@ -452,8 +310,8 @@ struct App : public OpenGLApplication
         for (unsigned int i = 0; i < N_TREES; ++i)
         {
             glm::mat4 treeMVP = projView * treeModelMatrices_[i];
-            glUniformMatrix4fv(transformMvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(treeMVP));
-            glUniform3f(transformColorModUniformLocation_, 1.0f, 1.0f, 1.0f);
+            glUniformMatrix4fv(mvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(treeMVP));
+            glUniform3f(colorModUniformLocation_, 1.0f, 1.0f, 1.0f);
             tree_.draw();
 		}
     }
@@ -462,15 +320,15 @@ struct App : public OpenGLApplication
     {
         glm::mat4 streetModel = glm::scale(glm::mat4(1), glm::vec3(MAP_SIZE, 0.0f, STREET_WIDTH));
         glm::mat4 streetMVP = projView * streetModel;
-        glUniformMatrix4fv(transformMvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(streetMVP));
-        glUniform3f(transformColorModUniformLocation_, 1.f, 1.0f, 1.0f);
+        glUniformMatrix4fv(mvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(streetMVP));
+        glUniform3f(colorModUniformLocation_, 1.f, 1.0f, 1.0f);
         street_.draw();
 
         glm::mat4 grassModel = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.1f, 0.0f));
         grassModel = glm::scale(grassModel, glm::vec3(MAP_SIZE, 1.0f, 50.0f));
         glm::mat4 grassMVP = projView * grassModel;
-        glUniformMatrix4fv(transformMvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(grassMVP));
-        glUniform3f(transformColorModUniformLocation_, 1.0f, 1.0f, 1.0f);
+        glUniformMatrix4fv(mvpUniformLocation_, 1, GL_FALSE, glm::value_ptr(grassMVP));
+        glUniform3f(colorModUniformLocation_, 1.0f, 1.0f, 1.0f);
         grass_.draw();
     }
 
@@ -513,7 +371,7 @@ struct App : public OpenGLApplication
         updateCameraInput();
         car_.update(deltaTime_);
 
-        glUseProgram(transformSP_);
+        glUseProgram(shaderProgram_);
 
         glm::mat4 projView = getPerspectiveProjectionMatrix() * getViewMatrix();
         drawGround(projView);
@@ -524,26 +382,10 @@ struct App : public OpenGLApplication
     }
 
 private:
-    // Shaders
-    GLuint basicSP_;
-    GLuint transformSP_;
-    GLuint basicColorModUniformLocation_;
-    GLuint basicMvpUniformLocation_;
-    GLuint transformColorModUniformLocation_;
-    GLuint transformMvpUniformLocation_;
+    GLuint shaderProgram_;
+    GLuint colorModUniformLocation_;
+    GLuint mvpUniformLocation_;
 
-    // Partie 1
-    GLuint vbo_, ebo_, vao_;
-
-    static constexpr unsigned int MIN_N_SIDES = 5;
-    static constexpr unsigned int MAX_N_SIDES = 12;
-
-    std::vector<Vertex> vertices_;
-    std::vector<GLuint> elements_;
-
-    int nSide_, oldNSide_;
-
-    // Partie 2
     Model tree_;
     Model streetlight_;
     Model grass_;
@@ -558,14 +400,6 @@ private:
     static constexpr unsigned int N_STREETLIGHTS = 5;
     glm::mat4 treeModelMatrices_[N_TREES];
     glm::mat4 streetlightModelMatrices_[N_STREETLIGHTS];
-
-    // Imgui var
-    const char* const SCENE_NAMES[2] = {
-        "Introduction",
-        "3D Model & transformation",
-    };
-    const int N_SCENE_NAMES = sizeof(SCENE_NAMES) / sizeof(SCENE_NAMES[0]);
-    int currentScene_;
 
     bool isMouseMotionEnabled_;
 	bool areTreesInitialized_ = false;
@@ -588,5 +422,5 @@ int main(int argc, char* argv[])
     settings.context.attributeFlags = sf::ContextSettings::Attribute::Core;
 
     App app;
-    app.run(argc, argv, "Tp1", settings);
+    app.run(argc, argv, "TP2", settings);
 }
