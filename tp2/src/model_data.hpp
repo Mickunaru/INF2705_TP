3e4983a53dcd00e20a49be488c9e3ef285f5c712
Arani Balasingam <arani.balasingam@polymtlus.ca>
2025-10-13 22:05:01 -0400

    Done

--- tp2/src/model_data.hpp
+++ tp2/src/model_data.hpp
@@ -3,14 +3,15 @@
 // TODO: Compléter les coordonnées de texture.
 //       On veut que les texture se répètent 10 fois sur le long du sol.
 //       Pour le sol, on aura aussi une répétition de 5 sur le large du sol.
-
+// 
+// GL_REPEAT in glTexParameteri
 float ground[] =
 {
-    // Position           // Texture coordinates
-    -0.5f, 0.0f, -0.5f,   0.0f, 0.0f,
-     0.5f, 0.0f, -0.5f,   0.0f, 0.0f,
-     0.5f, 0.0f,  0.5f,   0.0f, 0.0f,
-    -0.5f, 0.0f,  0.5f,   0.0f, 0.0f
+    // Position           // Texture coordinates (u, v)
+    -0.5f, 0.0f, -0.5f,   0.0f, 0.0f, // bottom-left
+     0.5f, 0.0f, -0.5f,   5.0f, 0.0f, // bottom-right (width repeat x5)
+     0.5f, 0.0f,  0.5f,   5.0f, 10.0f, // top-right (height repeat x10)
+    -0.5f, 0.0f,  0.5f,   0.0f, 10.0f //top-left
 };
 
 unsigned int planeElements[] =
@@ -21,10 +22,10 @@ unsigned int planeElements[] =
 
 float street[] =
 {
-    // Position           // Texture coordinates
-    -0.5f, 0.0f, -0.5f,   0.0f, 0.0f,
-     0.5f, 0.0f, -0.5f,   0.0f, 0.0f,
-     0.5f, 0.0f,  0.5f,   0.0f, 0.0f,
-    -0.5f, 0.0f,  0.5f,   0.0f, 0.0f
+    // Position           // Texture coordinates (u, v)
+    -0.5f, 0.0f, -0.5f,   0.0f, 0.0f, // bottom-left
+     0.5f, 0.0f, -0.5f,   1.0f, 0.0f, // bottom-right (width repeat x1)
+     0.5f, 0.0f,  0.5f,   1.0f, 10.0f, // top-right (length repeat x10)
+    -0.5f, 0.0f,  0.5f,   0.0f, 10.0f // top-left
 };
 
