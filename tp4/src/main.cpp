0994d5c788f8ede54a9dd9c19837a0ab2e447aeb
Michael Le <orangerasengan25@gmail.com>
2025-11-26 08:38:35 -0500

    Added tp4 init files

---
+++ tp4/src/main.cpp
@@ -0,0 +1,205 @@
+#include <cstddef>
+#include <cstdint>
+
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <imgui/imgui.h>
+
+#include <inf2705/OpenGLApplication.hpp>
+
+#define CHECK_GL_ERROR printGLError(__FILE__, __LINE__)
+
+using namespace gl;
+using namespace glm;
+
+
+struct App : public OpenGLApplication
+{
+    App()
+    : cameraPosition_(0.f, 0.f, 0.f)
+    , cameraOrientation_(0.f, 0.f)
+    , currentScene_(0)
+    , isMouseMotionEnabled_(false)
+    {
+    }
+	
+	void init() override
+	{
+		// Le message expliquant les touches de clavier.
+		setKeybindMessage(
+			"ESC : quitter l'application." "\n"
+			"T : changer de scène." "\n"
+			"W : déplacer la caméra vers l'avant." "\n"
+			"S : déplacer la caméra vers l'arrière." "\n"
+			"A : déplacer la caméra vers la gauche." "\n"
+			"D : déplacer la caméra vers la droite." "\n"
+			"Q : déplacer la caméra vers le bas." "\n"
+			"E : déplacer la caméra vers le haut." "\n"
+			"Flèches : tourner la caméra." "\n"
+			"Souris : tourner la caméra" "\n"
+			"Espace : activer/désactiver la souris." "\n"
+		);
+
+		// Config de base.
+	}
+
+	// Appelée à chaque trame. Le buffer swap est fait juste après.
+	void drawFrame() override
+	{
+        
+        ImGui::Begin("Scene Parameters");
+        ImGui::Combo("Scene", &currentScene_, SCENE_NAMES, N_SCENE_NAMES);
+        ImGui::End();
+        
+        switch (currentScene_)
+        {
+            case 0: sceneMain();  break;
+        }
+	}
+
+	// Appelée lorsque la fenêtre se ferme.
+	void onClose() override
+	{
+
+	}
+
+	// Appelée lors d'une touche de clavier.
+	void onKeyPress(const sf::Event::KeyPressed& key) override
+	{
+		using enum sf::Keyboard::Key;
+		switch (key.code)
+		{
+		    case Escape:
+		        window_.close();
+	        break;
+		    case Space:
+		        isMouseMotionEnabled_ = !isMouseMotionEnabled_;
+		        if (isMouseMotionEnabled_)
+		        {
+		            window_.setMouseCursorGrabbed(true);
+		            window_.setMouseCursorVisible(false);
+	            }
+	            else
+	            {
+	                window_.setMouseCursorGrabbed(false);
+	                window_.setMouseCursorVisible(true);
+                }
+	        break;
+	        case T:
+                currentScene_ = ++currentScene_ < N_SCENE_NAMES ? currentScene_ : 0;
+            break;
+		    default: break;
+		}
+	}
+
+	void onResize(const sf::Event::Resized& event) override
+	{	
+	}
+	
+	void onMouseMove(const sf::Event::MouseMoved& mouseDelta) override
+	{	    
+	    if (!isMouseMotionEnabled_)
+	        return;
+        
+        const float MOUSE_SENSITIVITY = 0.1;
+        float cameraMouvementX = mouseDelta.position.y * MOUSE_SENSITIVITY;
+        float cameraMouvementY = mouseDelta.position.x * MOUSE_SENSITIVITY;
+	    cameraOrientation_.y -= cameraMouvementY * deltaTime_;
+        cameraOrientation_.x -= cameraMouvementX * deltaTime_;
+	}
+	
+	void updateCameraInput() 
+    {
+        if (!window_.hasFocus())
+            return;
+            
+        if (isMouseMotionEnabled_)
+        {
+            sf::Vector2u windowSize = window_.getSize();
+            sf::Vector2i windowHalfSize(windowSize.x / 2.0f, windowSize.y / 2.0f);
+            sf::Mouse::setPosition(windowHalfSize, window_);
+        }
+        
+        float cameraMouvementX = 0;
+        float cameraMouvementY = 0;
+        
+        const float KEYBOARD_MOUSE_SENSITIVITY = 1.5f;
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+            cameraMouvementX -= KEYBOARD_MOUSE_SENSITIVITY;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+            cameraMouvementX += KEYBOARD_MOUSE_SENSITIVITY;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            cameraMouvementY -= KEYBOARD_MOUSE_SENSITIVITY;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            cameraMouvementY += KEYBOARD_MOUSE_SENSITIVITY;
+        
+        cameraOrientation_.y -= cameraMouvementY * deltaTime_;
+        cameraOrientation_.x -= cameraMouvementX * deltaTime_;
+
+        // Keyboard input
+        glm::vec3 positionOffset = glm::vec3(0.0);
+        const float SPEED = 10.f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            positionOffset.z -= SPEED;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            positionOffset.z += SPEED;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            positionOffset.x -= SPEED;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            positionOffset.x += SPEED;
+            
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
+            positionOffset.y -= SPEED;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::E))
+            positionOffset.y += SPEED;
+
+        positionOffset = glm::rotate(glm::mat4(1.0f), cameraOrientation_.y, glm::vec3(0.0, 1.0, 0.0)) * glm::vec4(positionOffset, 1);
+        cameraPosition_ += positionOffset * glm::vec3(deltaTime_);
+    }
+    
+    
+    void sceneMain()
+    {
+        
+    }
+    
+private:    
+    glm::vec3 cameraPosition_;
+    glm::vec2 cameraOrientation_;
+    
+    // Imgui var
+    const char* const SCENE_NAMES[1] = {
+        "Main Scene"
+    };
+    const int N_SCENE_NAMES = sizeof(SCENE_NAMES) / sizeof(SCENE_NAMES[0]);
+    int currentScene_;
+    
+    bool isMouseMotionEnabled_;
+};
+
+
+int main(int argc, char* argv[])
+{
+	WindowSettings settings = {};
+	settings.fps = 60;
+	settings.context.depthBits = 24;
+	settings.context.stencilBits = 8;
+	settings.context.antiAliasingLevel = 4;
+	settings.context.majorVersion = 3;
+	settings.context.minorVersion = 3;
+	settings.context.attributeFlags = sf::ContextSettings::Attribute::Core;
+
+	App app;
+	app.run(argc, argv, "Tp4", settings);
+}
